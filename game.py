c2e1880de5389397b021d37192bb8ec2228f7675
Evardanyan <64473803+Evardanyan@users.noreply.github.com>
2022-09-28 00:31:18 +0400

    Update game.py

--- game.py
+++ game.py
@@ -1,33 +1,80 @@
 # Write your code here
 def knight_move():
     try:
-        x, y = input("Enter the knight's starting position: ").split()
-        x = int(x)
-        y = int(y)
-        if (x > 8 or x < 0) or (y > 8 or y < 0):
-            print("Invalid dimensions!")
-        else:
-            show_cheeseboard(x, y)
+        while True:
+            x1, y1 = input("Enter your board dimensions: ").split()
+            x1 = int(x1)
+            y1 = int(y1)
+            if (x1 < 0 or x1 == 0) or (y1 < 0 or y1 == 0):
+                print("Invalid dimensions!")
+            else:
+                break
+        while True:
+            x, y = input("Enter the knight's starting position: ").split()
+            x1 = int(x1)
+            y1 = int(y1)
+            x = int(x)
+            y = int(y)
+
+            if (x > x1 or x <= 0) or (y > y1 or y <= 0):
+                print("Invalid position!")
+            else:
+                show_cheeseboard(x1, y1, x, y)
+                break
     except ValueError:
         print("Invalid dimensions")
 
 
-def show_cheeseboard(x, y):
-    cheese_board = [[" ", "-------------------"],
-                    [" 8|", "_", "_", "_", "_", "_", "_", "_", "_", "|"],
-                    [" 7|", "_", "_", "_", "_", "_", "_", "_", "_", "|"],
-                    [" 6|", "_", "_", "_", "_", "_", "_", "_", "_", "|"],
-                    [" 5|", "_", "_", "_", "_", "_", "_", "_", "_", "|"],
-                    [" 4|", "_", "_", "_", "_", "_", "_", "_", "_", "|"],
-                    [" 3|", "_", "_", "_", "_", "_", "_", "_", "_", "|"],
-                    [" 2|", "_", "_", "_", "_", "_", "_", "_", "_", "|"],
-                    [" 1|", "_", "_", "_", "_", "_", "_", "_", "_", "|"],
-                    [" ", "-------------------"],
-                    ["   ", "1", "2", "3", "4", "5", "6", "7", "8", ]]
-    cheese_board[9 - y][x] = "x"
-    for i in cheese_board:
+def show_cheeseboard(x, y, x1, y1):
+    if x > 9 or y > 9:
+        cheeseboard = [["___" for i in range(x + 2)] for j in range(y)]
+        cheeseboard[y - y1][x1] = "  X"
+        if y - y1 - 2 < y and x1 + 1 <= x and x1 - 1 <= x:
+            cheeseboard[y - y1 - 2][x1 + 1] = " O"
+            cheeseboard[y - y1 - 2][x1 - 1] = " O"
+        if y - y1 + 2 < y and x1 + 1 <= x and x1 - 1 <= x:
+            cheeseboard[y - y1 + 2][x1 + 1] = " O"
+            cheeseboard[y - y1 + 2][x1 - 1] = " O"
+        if y - y1 - 1 < y and x1 + 2 <= x and x1 - 2 <= x:
+            cheeseboard[y - y1 - 1][x1 + 2] = " O"
+            cheeseboard[y - y1 - 1][x1 - 2] = " O"
+        if y - y1 + 1 < y and x1 + 2 <= x and x1 - 2 <= x:
+            cheeseboard[y - y1 + 1][x1 + 2] = " O"
+            cheeseboard[y - y1 + 1][x1 - 2] = " O"
+    else:
+        cheeseboard = [["__" for i in range(x + 2)] for j in range(y)]
+        cheeseboard[y - y1][x1] = " X"
+        if y > y - y1 - 2 >= 0 and x1 + 1 <= x and x1 - 1 <= x:
+            cheeseboard[y - y1 - 2][x1 + 1] = " O"
+            cheeseboard[y - y1 - 2][x1 - 1] = " O"
+        if y > y - y1 + 2 >= 0 and x1 + 1 <= x and x1 - 1 <= x:
+            cheeseboard[y - y1 + 2][x1 + 1] = " O"
+            cheeseboard[y - y1 + 2][x1 - 1] = " O"
+        if y > y - y1 - 1 >= 0 and x1 + 2 <= x and x1 - 2 <= x:
+            cheeseboard[y - y1 - 1][x1 + 2] = " O"
+            cheeseboard[y - y1 - 1][x1 - 2] = " O"
+        if y > y - y1 + 1 >= 0 and x1 + 2 <= x and x1 - 2 <= x:
+            cheeseboard[y - y1 + 1][x1 + 2] = " O"
+            cheeseboard[y - y1 + 1][x1 - 2] = " O"
+    new_val = [y - i for i in range(y)]
+    for new_val, subList in zip(new_val, cheeseboard):
+        subList[0] = f"{new_val}|"
+        subList[x + 1] = f"|"
+    print("", ((x * 3) + 3) * "-")
+    count = y
+    for i in cheeseboard:
         clean_comma = ", ".join(i)
         print(clean_comma.replace(", ", " "))
+        count -= 1
+    print("", ((x * 3) + 3) * "-")
+    horizontal_number = "    "
+    for i in range(1, x + 1):
+        horizontal_number = horizontal_number + str(i) + "  "
+    horizontal_number.rstrip()
+    print(horizontal_number)
 
 
+
+
+# show_cheeseboard(4, 4, 2, 2)
 knight_move()
